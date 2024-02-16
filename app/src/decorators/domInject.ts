3bdc30abeeffce0037f69ff632c61a5e1244cb61
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-02-16 15:56:39 -0300

    update

--- app/src/decorators/domInject.ts
+++ app/src/decorators/domInject.ts
@@ -1,16 +1,6 @@
-export function domInject(select: string) {
-    return function (
-        target: any,
-        propetyKey: string,
-        descriptor : PropertyDescriptor
-    ) {
-        const ModeloOriginal = descriptor.value;
-        descriptor.value = function(...args: any[]) {
-            const retorno = descriptor.value;
-            
-            return retorno;
-        }
-    
-        return descriptor;
+export function domInject(seletor: string) {
+    return function ( target: any, propetyKey: string ) {
+        
+    }
 }
-}
+
